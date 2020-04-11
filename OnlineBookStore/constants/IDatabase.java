d3e97e9c7c4fa8a6cb6e7623701085712aef47f7
sandeeprasath <57119297+sandeeprasath@users.noreply.github.com>
2020-04-10 21:45:10 -0500

    updated URL

--- OnlineBookStore/constants/IDatabase.java
+++ OnlineBookStore/constants/IDatabase.java
@@ -2,9 +2,9 @@ package constants;
 
 public class IDatabase {
 
-	public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-	public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/onlinebookstore";
-	public static final String USER_NAME = "root";
-	public static final String PASSWORD = "root";
+	public static final String DRIVER_NAME = "org.postgresql.Driver";
+	public static final String CONNECTION_STRING = "jdbc:postgresql://18.224.246.100:5432/onlinebookstore";
+	public static final String USER_NAME = "postgre";
+	public static final String PASSWORD = "password";
 
 }
